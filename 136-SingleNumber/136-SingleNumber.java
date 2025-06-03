3b567dc4fb67ae3419e3b34a48f7bdaf4fe288fb
Deepesh_np <nrndpsh@gmail.com>
2025-06-03 09:54:45 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 03/06/2025, 09:54:44
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        return xor;
+    }
+}
