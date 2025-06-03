36914824f054afe81544b3efb5c7291b1d834fc6
Deepesh_np <nrndpsh@gmail.com>
2025-06-03 09:55:01 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.cpp
@@ -0,0 +1,20 @@
+// Last updated: 03/06/2025, 09:55:01
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        long bForm=n ;
+        if ( n<0 )
+        {
+            x=1/x;
+            bForm= -bForm;
+        } 
+        while ( bForm > 0){
+            if ( bForm % 2 == 1){
+            ans*=x;}
+            x*=x;
+            bForm= bForm/2;
+        }
+        return ans;
+    }
+};
